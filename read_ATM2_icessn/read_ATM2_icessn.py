9fdbb2287e9fa6d89db50865dfdca3b4bc93504e
tsutterley <tsutterl@uw.edu>
2022-01-20 15:40:30 -0800

    fix: update regex for OIB ATM 2019 files

--- read_ATM2_icessn/read_ATM2_icessn.py
+++ read_ATM2_icessn/read_ATM2_icessn.py
@@ -97,7 +97,7 @@ def read_ATM2_icessn(full_filename, SUBSETTER=None, VARIABLES=None):
     regex=re.compile(r'[-+]?(?:(?:\d*\.\d+)|(?:\d+\.?)|(?:\*+))(?:[Ee][+-]?\d+)?')
     #-- read the input file, split at lines and remove all commented lines
     with open(os.path.expanduser(full_filename),'r') as f:
-        file_contents=[i for i in f.read().splitlines() if re.match(r'^(?!#)',i)]
+        file_contents=[i for i in f.readlines() if re.match(r'^(?!\#|\n)',i)]
     #-- subset the data to indices if specified
     if SUBSETTER:
         file_contents = [file_contents[i] for i in SUBSETTER]
